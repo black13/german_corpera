78c4cfece92a545efc2eb72c1aadd05157193ced
jjosburn@gmail.com <jjosburn@gmail.com>
2019-06-10 23:15:02 -0500

    long verblist

--- scrapverbs.py
+++ scrapverbs.py
@@ -6,7 +6,7 @@ import itertools
 import requests
 from itertools import groupby
 from itertools import dropwhile,takewhile
-
+from verblist import verb_list
 def group2(iterator, count):
     return list(itertools.imap(None, *([ iter(iterator) ] * count)))
 
@@ -26,10 +26,15 @@ def get_meaning(parent):
 
 def get_conjugation(parent):
     tag_list = [x for x in parent.find_next_siblings()]
-    filt=list(dropwhile(lambda t: 'Conjugation[edit]' not in t.text,tag_list))       
-    filt = [x for x in filt if x.find('table') is not None]
-    filt=filt[0]
-    table=filt.find('table')
+    filt=list(dropwhile(lambda t: 'Conjugation[edit]' not in t.text,tag_list))  
+    tables=[x for x in filt if x.findAll('table')]
+    #print(tables)
+    #filt=list(dropwhile(lambda t: 'div' not in t.name,tag_list))   
+    #filt = [x for x in filt if x.find('table') is not None]
+ 
+    table=[ x for x in [x.find('table') for x in tables] if x.has_attr('class')][0]
+    #tables = [x for x in filt if not 'A user suggests' in x.text]
+    #print(table)
     table.find_all('trs',{})
     table_body = table.find('tbody')
 
@@ -51,9 +56,9 @@ def get_conjugation(parent):
     return verbs
 line_parbox='''\\parbox[t][][t]{{2cm}}{{{ich}\\\\{du}\\\\{er}\\\\{wir}\\\\{ihr}\\\\{sie}}}'''
 table_verb='''%==={word}===
-\\card{{\sffamily \huge {word}}}{{
+\\card{{\Huge {word}}}{{
 \\begin{{tabular}}{{lll}}
-\\parbox[t][][t]{{1.5cm}}{{\\raggedleft ich\\\\du\\\\er/sie/es\\\\wir\\\\ihr\\\\sie}} &	
+\\parbox[t][][t]{{1.5cm}}{{\\raggedleft ich\\\\du\\\\er/sie/es\\\\wir\\\\ihr\\\\sie}} & 
 {present} &
 {past}\\\\
 \\end{{tabular}}
@@ -61,21 +66,6 @@ table_verb='''%==={word}===
 
 def main():
     try:
-        verb_list=['warten','arbeiten','bekommen','bleiben','brauchen',
-        'bringen','denken','essen','fahren',
-        'finden','fragen','geben','gehen',
-        'gehören','glauben','haben','halten',
-        'heißen','helfen','hören','kaufen',
-        'kennen','kommen','können','lassen',
-        'laufen','leben','legen','liegen',
-        'machen','mögen','müssen','nehmen',
-        'sagen','schlafen','schlagen','schreiben',
-        'sehen','sein','sitzen','sollen',
-        'spielen','sprechen','stehen','stellen',
-        'sterben','tragen','treffen',
-        'tun','warten','werden','werfen',
-        'wissen','wohnen','wollen','ziehen']
-
         #shuffle(verb_list)
         cards={}
         for verb in verb_list:
@@ -83,8 +73,12 @@ def main():
             container = soup.find('span',{'id':"German",'class':"mw-headline"})
             if container:
                 parent=container.parent
+                meaning=get_meaning(parent)
+                meaning=[x.split("\n") for x in meaning]
+                #meaning=meaning.split('\n')
+                print(meaning)
                 verblist=get_conjugation(parent)
-                #print(verblist)
+                
                 present=list(itertools.chain.from_iterable([x[0:2] for x in verblist][0:3]))
                 past=list(itertools.chain.from_iterable([x[0:2] for x in verblist][3:6]))
                 
@@ -104,7 +98,7 @@ def main():
                 cards[verb]=table_verb.format(**out)
                 #print(l)
                 #print(table_verb.format(**l))
-                #get_meaning(parent)
+                
 
     except Exception as e:
         print('Error on line {}'.format(sys.exc_info()[-1].tb_lineno), type(e).__name__, e)    
