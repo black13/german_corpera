3cf6a74dc85f8cda8059891bf0054216c4ae109f
jjosburn@gmail.com <jjosburn@gmail.com>
2019-06-24 00:38:02 -0500

    better

--- pons.py
+++ pons.py
@@ -26,12 +26,24 @@ table_verb='''
 \\begin{{tabular}}{{l}}
 \\parbox[t][][t]{{8cm}}{{}}\\\\
 \\parbox[t][][t]{{8cm}}{{\\normalfont \\footnotesize 
-{{example}} 
+{example} 
 }}\\\\
 \\end{{tabular}}
 }}'''
 
-def make_request(verb):
+header='''\\documentclass[a4paper,backgrid,frontgrid]{flacards}
+\\usepackage{array,booktabs,tabularx}
+\\usepackage[condensed,sfdefault]{universalis}
+\\usepackage[T1]{fontenc}
+\\begin{document}
+\\pagesetup{2}{4}
+
+'''
+
+footer='''
+\\end{document}
+'''
+def make_request_conjugation(verb):
     r=None
     headers = {'user-agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/53.0.2785.143 Safari/537.36'}
     try:
@@ -44,16 +56,25 @@ def make_request(verb):
         print('Error on line {}'.format(sys.exc_info()[-1].tb_lineno), type(e).__name__, e)
     return r
 
-
-def parse(verb):
-
-    
-    text=""
-    ret={}
-    conjugations=['Präsens','Präteritum']
-    #conjugations=['Präsens','Präteritum','Perfekt']
+def make_request(verb):
+    r=None
+    headers = {'user-agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/53.0.2785.143 Safari/537.36'}
     try:
-        r=make_request(verb)
+        url="https://en.pons.com/translate?q={0}&l=deen&in=&lf=de&qnac=".format(verb)   
+        #centralproxy.northgrum.com
+        proxies = {'http': '134.223.230.151:80','https': '134.223.230.151:80'}
+        r = requests.get(url, headers=headers,verify=False,timeout=100)
+        sleep(2)
+    except Exception as e:
+        print('Error on line {}'.format(sys.exc_info()[-1].tb_lineno), type(e).__name__, e)
+    return r
+    
+def parse(verb):
+    text=""
+    out={}
+    conjugations=['Präsens','Präteritum']
+    try:
+        r=make_request_conjugation(verb)
 
         if r.status_code == 200:
             print('Processing..' + verb)
@@ -64,22 +85,28 @@ def parse(verb):
             
             '''out=[process_table(table.find(text=x).parent.nextSibling) for x in conjugations]'''
             tables={x:table.find(text=x).parent.nextSibling for x in conjugations}
-            out={}
             out['word']=verb
             for key,table in tables.items():
                 tds =[[x.text for x in y.findAll('td')] for y in table.findAll('tr')]
                 ret={x[0]:" ".join(x[1:]) for x in tds}
                 out[key]=line_parbox.format(**ret)
-            text=table_verb.format(**out)     
-            '''
+        
+        r=make_request(verb)
+        if r.status_code == 200:
+            html = r.text
+            
+            soup = BeautifulSoup(html, "html.parser")
+            table=soup.find('div',{'id':"flection_table","class":"de"})
             sel=soup.select('dl[data-translation]')
             textlist=list(chain.from_iterable([x.split('\n') for x in [re.sub(r'\n+','\n',x.text.strip()) for x in sel]]))
             textlist=[x for x in textlist if 'British English American English' not in x ]
             textlist=[x for x in [x.strip() for x in textlist] if len(x) > 0]
             textlist=[re.sub(r'\[','(',x) for x in textlist]
             textlist=[re.sub(r'\]',')',x) for x in textlist]
-            out=table_verb.format(**{'word':verb,'example':"\n".join(textlist[0:10])})
-            '''
+            out['example']=" - \n".join(textlist[0:10])
+            text=table_verb.format(**out)
+            
+            
             '''
             textlist=[x.split('\n') for x in [x.text.strip() for x in sel] ]
             textlist=[x for x in textlist if len(x) > 0]
@@ -110,9 +137,10 @@ def main():
     p.join()
 
     f=open("pons-check.tex","w",encoding='utf-8')
+    f.write(header)
     for record in records:
         f.write (record)
-    f.close()
+    f.write(footer)
     
 if __name__ == "__main__":
     main()
